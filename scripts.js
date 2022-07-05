aee9e0efae83a14b81f8877d93b6f59dd40a10be
Miran-Mirantee <killerm11805@hotmail.com>
2022-07-05 13:29:04 +0700

    Implemented negate function and Implemented clear function

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@ let num2;
 let temp = [];
 let num1Rdy = false;
 let num2Rdy = false;
+let negateOn = false;
 let operator;
 
 function negate(num) {
@@ -25,11 +26,20 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
+function reset() {
+    num1 = undefined;
+    num2 = undefined;
+    num1Rdy = false;
+    num2Rdy = false;
+    negateOn = false;
+    temp = [];
+    showDisplay('');
+}
+
 // num1 = the first number to use with operator
 // num2 = the secode number to use with operator
 // operator = the operator to calculate the result
 function operate(num1, num2, operator) {
-    num2 = parseInt(temp.join(''));
     if (num2 === 0 && operator == divide) {
         num1Rdy = false;
         num2Rdy = false;
@@ -53,16 +63,27 @@ function showDisplay(num) {
 const equalBtn = document.querySelector('.equal');
 equalBtn.addEventListener('click', () => {
     // check if the number#1 and number#2 is ready to calculate then calculate
-    if (num1Rdy && num2Rdy) 
+    if (num1Rdy && num2Rdy) {
         num1 = operate(num1, num2, operator);
+        negateOn = false;
+    }
     else
         console.log(`enter num2 first`)
 });
 
 const negateBtn = document.querySelector('.negate');
-negateBtn.addEventListener('click', () => [
-
-]);
+negateBtn.addEventListener('click', () => {
+    if (!num1Rdy && num1) {
+        num1 = negate(num1);
+        negateOn = true;
+        showDisplay(num1);
+    }
+    else if (num1Rdy && num2Rdy) {
+        num2 = negate(num2);
+        negateOn = true;
+        showDisplay(num2);
+    }
+});
 
 const addBtn = document.querySelector('.add');
 addBtn.addEventListener('click', () => {
@@ -71,8 +92,10 @@ addBtn.addEventListener('click', () => {
         temp = [];
         operator = add;
         num1Rdy = true;
+        negateOn = false;
     }
     // check if the number#1 but number#2 is NOT ready then assign operator as add
+    // in case the user want to change the operator 
     else if (num1Rdy && !num2Rdy) {
         operator = add;
     }
@@ -91,8 +114,10 @@ subtractBtn.addEventListener('click', () => {
         temp = [];
         operator = subtract;
         num1Rdy = true;
+        negateOn = false;
     }
     // check if the number#1 but number#2 is NOT ready then assign operator as subtract
+    // in case the user want to change the operator 
     else if (num1Rdy && !num2Rdy) {
         operator = subtract;
     }
@@ -111,8 +136,10 @@ multiplyBtn.addEventListener('click', () => {
         temp = [];
         operator = multiply;
         num1Rdy = true;
+        negateOn = false;
     }
     // check if the number#1 but number#2 is NOT ready then assign operator as multiply
+    // in case the user want to change the operator 
     else if (num1Rdy && !num2Rdy) {
         operator = multiply;
     }
@@ -131,8 +158,10 @@ divideBtn.addEventListener('click', () => {
         temp = [];
         operator = divide;
         num1Rdy = true;
+        negateOn = false;
     }
     // check if the number#1 but number#2 is NOT ready then assign operator as divide
+    // in case the user want to change the operator 
     else if (num1Rdy && !num2Rdy) {
         operator = divide;
     }
@@ -147,12 +176,7 @@ divideBtn.addEventListener('click', () => {
 const clearBtn = document.querySelector('.clear');
 clearBtn.addEventListener('click', () => {
     // Reset everything
-    num1 = undefined;
-    num2 = undefined;
-    num1Rdy = false;
-    num2Rdy = false;
-    temp = [];
-    showDisplay('');
+    reset();
 });
 
 const backspaceBtn = document.querySelector('.backspace');
@@ -161,6 +185,8 @@ backspaceBtn.addEventListener('click', () => {
     if (!num1Rdy) {
         temp.pop();
         num1 = parseInt(temp.join(''));
+        if (negateOn)
+            num1 = negate(num1);
         if (!num1)
             showDisplay('');
         else
@@ -171,6 +197,8 @@ backspaceBtn.addEventListener('click', () => {
         temp.pop();
         num2 = parseInt(temp.join(''));
         num2Rdy = true;
+        if (negateOn)
+            num2 = negate(num2);
         if (!num2) {
             showDisplay('');
             num2Rdy = false;
@@ -187,14 +215,18 @@ for (const number of numbers) {
         if (!num1Rdy) {
             temp.push(number.textContent);
             num1 = parseInt(temp.join(''));
-            showDisplay(parseInt(temp.join('')));
+            if (negateOn)
+                num1 = negate(num1);
+            showDisplay(num1);
         }
         // check if number#1 is ready then assign input into number#2 instead
         if (num1Rdy) {
             temp.push(number.textContent);
             num2 = parseInt(temp.join(''));
             num2Rdy = true;
-            showDisplay(parseInt(temp.join('')));
+            if (negateOn)
+                num2 = negate(num2);
+            showDisplay(num2);
         }
     });
 }
