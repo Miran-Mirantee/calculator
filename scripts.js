405c280158ca035c25e1dbcf41dc0615d6cdc4f7
Miran-Mirantee <killerm11805@hotmail.com>
2022-07-05 23:40:13 +0700

    Comments added on toFixed() function

--- scripts.js
+++ scripts.js
@@ -84,6 +84,7 @@ function getNum(num) {
     }
 }
 
+// to fix floating number problem
 function toFixed(value, precision) {
     var power = Math.pow(10, precision || 0);
     return String(Math.round(value * power) / power);
